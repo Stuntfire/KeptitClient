13992737ce867ebd6509dbe9211f05f817ebf6a7
Casper Sørensen <casp677d@edu.easj.dk>
2017-04-26 10:43:06 +0200

    Addede Taskhandler klassen

--- KeptitClient/Handlers/TaskHandler.cs
+++ KeptitClient/Handlers/TaskHandler.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KeptitClient.ViewModels
+namespace KeptitClient.Handlers
 {
-    public class MainViewModel
+   public class TaskHandler
     {
-        public MainViewModel()
+        public TaskHandler()
         {
 
         }
