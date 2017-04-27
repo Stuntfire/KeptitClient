012f5250e6a14c1077c8e0378a5b28250e0824ce
rolf3d <rolf0585@edu.easj.dk>
2017-04-27 14:30:29 +0200

    Tilføjet LoadAreaAync og LoadSubAreaAsync i persistency

--- KeptitClient/Persistency/PersistencyService.cs
+++ KeptitClient/Persistency/PersistencyService.cs
@@ -15,53 +15,75 @@ namespace KeptitClient.Persistency
     public class PersistencyService
     {
         //const string til serverenx
-        const string serverURL = "http://keptitwebservice.azurewebsites.net";
+        const string serverUrl = "http://keptitwebservice.azurewebsites.net";
 
-        
+
 
         //Tilføj en udført opgave.
-        public static void PostAsJsonTask(FinishedTask TaskToPost)
+        //public static void PostAsJsonTask(FinishedTask TaskToPost)
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.BaseAddress = new Uri(serverUrl);
+        //        client.DefaultRequestHeaders.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        //        try
+        //        {
+        //            var response = client.PostAsJsonAsync<FinishedTask>("api/finishedtasks", TaskToPost).Result;
+
+        //            if (response.IsSuccessStatusCode)
+        //            {
+        //                MessageDialogHelper.Show("Udført opgave blev gemt", TaskToPost.FinishedTasksID.ToString());
+        //            }
+        //            else
+        //            {
+        //                MessageDialogHelper.Show("Udført opgave blev ikke gemt", TaskToPost.FinishedTasksID.ToString());
+        //            }
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            MessageDialogHelper.Show("Fejl ", TaskToPost.FinishedTasksID.ToString());
+        //        }
+
+        //    }
+        //}
+
+        // Henter alle Area fra tabellen Areas
+        public static ObservableCollection<Area> LoadAreasAsync()
         {
+
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri(serverURL);
+                client.BaseAddress = new Uri(serverUrl);
                 client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string urlString = "api/areas";
 
-                try
+                HttpResponseMessage response = client.GetAsync(urlString).Result;
+                if (response.IsSuccessStatusCode)
                 {
-                    var response = client.PostAsJsonAsync<FinishedTask>("api/finishedtasks", TaskToPost).Result;
-
-                    if (response.IsSuccessStatusCode)
-                    {
-                        MessageDialogHelper.Show("Udført opgave blev gemt", TaskToPost.FinishedTasksID.ToString());
-                    }
-                    else
-                    {
-                        MessageDialogHelper.Show("Udført opgave blev ikke gemt", TaskToPost.FinishedTasksID.ToString());
-                    }
+                    var arealiste = response.Content.ReadAsAsync<ObservableCollection<Area>>().Result;
+                    return arealiste;
                 }
-                catch (Exception e)
-                {
-                    MessageDialogHelper.Show("Fejl ", TaskToPost.FinishedTasksID.ToString());
-                }
-
+                return null;
             }
         }
 
-        public static ObservableCollection<Area> GetArea()
+        // Henter alle SubArea fra tabellen SubAreas
+        public static ObservableCollection<SubArea> LoadSubAreasAsync()
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri(serverURL);
-                var response = client.GetAsync("api/areas").Result;
+                client.BaseAddress = new Uri(serverUrl);
+                client.DefaultRequestHeaders.Clear();
+                string urlString = "api/subareas";
 
+                HttpResponseMessage response = client.GetAsync(urlString).Result;
                 if (response.IsSuccessStatusCode)
                 {
-                    var areaListe = response.Content.ReadAsAsync<ObservableCollection<Area>>().Result;
-                    return areaListe;
+                    var subarealiste = response.Content.ReadAsAsync<ObservableCollection<SubArea>>().Result;
+                    return subarealiste;
                 }
-
                 return null;
             }
         }
