d99977b204a0d5507611bca7ea8ebb10d250f763
rolf3d <rolf0585@edu.easj.dk>
2017-05-01 12:20:34 +0200

    Ny klasse til GreenkeeperInfo, så den passer med Webservicen

---
+++ KeptitClient/Models/GreenkeeperInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeptitClient.Models
+{
+    public class GreenkeeperInfo
+    {
+        public string GreenkeeperName { get; set; }
+        
+        public string AreaTitle { get; set; }
+
+      
+        public string SubAreaTitle { get; set; }
+
+       
+        public string GreenTaskTitle { get; set; }
+
+      
+        public int Hours { get; set; }
+
+      
+        public int Minutes { get; set; }
+
+    
+        public DateTime Date { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
