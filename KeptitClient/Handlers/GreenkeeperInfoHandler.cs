1a95bd1b447dd9abd0487407b2cc4d201ae5a71f
Søren Roed Schack <soer622t@edu.easj.dk>
2017-06-06 12:03:10 +0200

    Ny kortere LINQ sætning der sorterer opgaverne efter dato.

--- KeptitClient/Handlers/GreenkeeperInfoHandler.cs
+++ KeptitClient/Handlers/GreenkeeperInfoHandler.cs
@@ -22,47 +22,27 @@ namespace KeptitClient.Handlers
         {
             Mwm.GreenkeeperInfoCollection.Clear();
 
-            var altInfoListe =
-                from i in await PersistencyService.LoadGreenkeeperInfoAsync()
-                where i.GreenkeeperName != ""
-                orderby i.Date descending
-                group i by new { i.GreenkeeperName, i.Date }
-                into infoliste
-                select new
-                {
-                    d = infoliste.Key,
-                    Name = infoliste.Min(x => x.GreenkeeperName),
-                    Data = infoliste.Min(x => x.Date),
-                    AreaTitle = infoliste.Min(x => x.AreaTitle),
-                    Hours = infoliste.Min(x => x.Hours),
-                    Notes = infoliste.Min(x => x.Notes),
-                    GreenTaskTitle = infoliste.Min(x => x.GreenTaskTitle),
-                    Minutes = infoliste.Min(x => x.Minutes)
+            var OrderByDate =
+                from obd in await PersistencyService.LoadGreenkeeperInfoAsync()
+                orderby obd.Date descending
+                select obd;
 
-
-                };
-
-            // Viser listen uden {} = og andre unødvændige variabel navne.
-            var GreenkeeperinfoListen =
-                from b1 in altInfoListe
-                orderby b1.d.Date descending
-                select new Models.GreenkeeperInfo() { GreenkeeperName = b1.Name, Date = b1.Data, AreaTitle = b1.AreaTitle, Hours = b1.Hours, Notes = b1.Notes, GreenTaskTitle = b1.GreenTaskTitle, Minutes = b1.Minutes };
-
-
-            //Brug foreach hvis LoadSubAreaAsync() i PersistencyService kodes som async:
-            foreach (var item in GreenkeeperinfoListen)
+            foreach (var item in OrderByDate)
             {
                 Mwm.GreenkeeperInfoCollection.Add(item);
             }
-
-            
         }
 
         public async Task LoadUpdatedList()
         {
             Mwm.GreenkeeperInfoCollection.Clear();
 
-            foreach (var item in await PersistencyService.LoadGreenkeeperInfoAsync())
+            var OrderTasksByDate =
+                from o in await PersistencyService.LoadGreenkeeperInfoAsync()
+                orderby o.Date descending
+                select o;
+
+            foreach (var item in OrderTasksByDate)
             {
                 if (item.GreenkeeperName == Mwm.SelectedGreenKeeper.GreenkeeperName)
                 {
